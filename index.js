a9a4e6a6b1a946b07f9f59b0a65df516045da0a2
johyunyoung <lucky7jhy@gmail.com>
2021-02-23 12:41:15 +0900

    almost done!

--- index.js
+++ index.js
@@ -5,21 +5,77 @@ const rightField = document.querySelector(".rightField");
 
 let x = canvas.width/2;
 let y = canvas.height-30;
-let dx = -2;
-let dy = -2;
+let dx = Math.random() * 6 - 3;
+let dy = -Math.random() * 2 - 1;
 const ballRadius = 10;
 const paddleHeight = 10;
 const paddleWidth = 75;
 let paddleX = (canvas.width-paddleWidth)/2;
 let rightPressed = false;
 let leftPressed = false;
+let brickRowCount = 4;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+for(let c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for(let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = {
+            x: 0,
+            y: 0,
+            status: 1
+        };
+    }
+}
 
 let SPEED = 1;
 let COLOR = "#0095DD";
+let PADDLE_COLOR = "#0095DD";
 
-function changeColor(){
+function changePaddleColor(){
+    PADDLE_COLOR = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+}
+
+function changeBallColor(){
     COLOR = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
-    console.log(ctx.fillStyle);
+}
+
+function collisionDetection() {
+    for(let c = 0; c < brickColumnCount; c++) {
+        for(let r = 0; r < brickRowCount; r++) {
+            let b = bricks[c][r];
+            if(b.status === 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    changePaddleColor();
+                }
+            }
+        }
+    }
+}
+
+function drawBricks() {
+    for(let c = 0; c < brickColumnCount; c++) {
+        for(let r = 0; r < brickRowCount; r++) {
+            if(bricks[c][r].status === 1) {
+                let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = PADDLE_COLOR;
+                ctx.fill();
+                ctx.closePath();                
+            }
+        }
+    }
 }
 
 function drawBall() {
@@ -33,31 +89,33 @@ function drawBall() {
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = PADDLE_COLOR;
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
     x += dx * SPEED;
     y += dy * SPEED;
 
     if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
-        changeColor();
+        changeBallColor();
     }
 
     if(y + dy < ballRadius) {
         dy = -dy;
-        changeColor();
+        changeBallColor();
     } else if(y + dy > canvas.height - ballRadius) {
         if(x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
-            changeColor();
-            SPEED += 0.2;
+            changeBallColor();
+            SPEED += 0.1;
             console.log(SPEED);
         } else {
             // alert("GAME OVER");
